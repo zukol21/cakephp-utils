d339f81c9d157d7b858564ba6b193e081b5dc9a6
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-09-02 01:46:32 +0300

    Added assoc array wrapping to MigrationParser (task #2441)

--- Csv/MigrationParserTest.php
+++ Csv/MigrationParserTest.php
@@ -15,4 +15,24 @@ class MigrationParserTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(is_array($result), "Parser returned a non-array");
         $this->assertFalse(empty($result), "Parser returned empty result");
     }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWrapFromPathException()
+    {
+        $file = dirname(dirname(dirname(__DIR__))) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS . 'Foo' . DS . 'migration.dist.csv';
+        $parser = new MigrationParser();
+        $result = $parser->wrapFromPath($file, [], 'foobar');
+    }
+
+    public function testWrapFromPath()
+    {
+        $file = dirname(dirname(dirname(__DIR__))) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS . 'Foo' . DS . 'migration.dist.csv';
+        $parser = new MigrationParser();
+        $result = $parser->wrapFromPath($file);
+
+        $this->assertTrue(is_array($result), "Parser returned a non-array");
+        $this->assertFalse(empty($result), "Parser returned empty result");
+    }
 }
