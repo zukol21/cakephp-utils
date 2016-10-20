8c210b664d2d365aaba314400f11c08f96c60ba2
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-10-20 04:28:33 +0300

    Implemented basic PathFinder classes (task #2833)
    * Added PathFinderInterface
    * Added BasePathFinder
    * Added ConfigPathFinder
    * Added MigrationPathFinder
    * Added unit tests for the above

---
+++ ConfigPathFinderTest.php
@@ -0,0 +1,77 @@
+<?php
+namespace CsvMigrations\Test\TestCase\PathFinder;
+
+use Cake\Core\Configure;
+use CsvMigrations\PathFinder\ConfigPathFinder;
+use PHPUnit_Framework_TestCase;
+
+class ConfigPathFinderTest extends PHPUnit_Framework_TestCase
+{
+    protected $pf;
+
+    protected function setUp()
+    {
+        $this->pf = new ConfigPathFinder();
+        $dir = dirname(dirname(__DIR__)) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS;
+        Configure::write('CsvMigrations.migrations.path', $dir);
+    }
+
+    public function testInterface()
+    {
+        $implementedInterfaces = array_keys(class_implements($this->pf));
+        $this->assertTrue(in_array('CsvMigrations\PathFinder\PathFinderInterface', $implementedInterfaces), "PathFinderInterface is not implemented");
+    }
+
+    public function testFind()
+    {
+        $path = $this->pf->find('Foo');
+        $this->assertFalse(empty($path), "Path is empty [$path]");
+        $this->assertTrue(is_string($path), "Path is not a string [$path]");
+        $this->assertTrue(file_exists($path), "Path does not exist [$path]");
+        $this->assertTrue(is_readable($path), "Path is not readable [$path]");
+        $this->assertTrue(is_file($path), "Path is not a file [$path]");
+    }
+
+    public function testFindOther()
+    {
+        $path = $this->pf->find('Foo', 'other_config.ini');
+        $this->assertFalse(empty($path), "Path is empty [$path]");
+        $this->assertTrue(is_string($path), "Path is not a string [$path]");
+        $this->assertTrue(file_exists($path), "Path does not exist [$path]");
+        $this->assertTrue(is_readable($path), "Path is not readable [$path]");
+        $this->assertTrue(is_file($path), "Path is not a file [$path]");
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionModuleEmpty()
+    {
+        $path = $this->pf->find();
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionModuleNotString()
+    {
+        $path = $this->pf->find(['foo' => 'bar']);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionPathNotString()
+    {
+        $path = $this->pf->find('Foo', ['foo' => 'bar']);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionPathNotExist()
+    {
+        $path = $this->pf->find('Foo', 'some_non_existing_file.ini');
+    }
+
+}
---
+++ MigrationPathFinderTest.php
@@ -0,0 +1,79 @@
+<?php
+namespace CsvMigrations\Test\TestCase\PathFinder;
+
+use Cake\Core\Configure;
+use CsvMigrations\PathFinder\MigrationPathFinder;
+use PHPUnit_Framework_TestCase;
+
+class MigrationPathFinderTest extends PHPUnit_Framework_TestCase
+{
+    protected $pf;
+
+    protected function setUp()
+    {
+        $this->pf = new MigrationPathFinder();
+        $dir = dirname(dirname(__DIR__)) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS;
+        Configure::write('CsvMigrations.migrations.path', $dir);
+    }
+
+    public function testInterface()
+    {
+        $implementedInterfaces = array_keys(class_implements($this->pf));
+        $this->assertTrue(in_array('CsvMigrations\PathFinder\PathFinderInterface', $implementedInterfaces), "PathFinderInterface is not implemented");
+    }
+
+    /*
+    public function testFind()
+    {
+        $path = $this->pf->find('Foo');
+        $this->assertFalse(empty($path), "Path is empty");
+        $this->assertTrue(is_string($path), "Path is not a string");
+        $this->assertTrue(file_exists($path), "Path does not exist");
+        $this->assertTrue(is_readable($path), "Path is not readable");
+        $this->assertTrue(is_file($path), "Path is not a file");
+    }
+     */
+
+    public function testFindOther()
+    {
+        $path = $this->pf->find('Foo', 'migration.dist.csv');
+        $this->assertFalse(empty($path), "Path is empty");
+        $this->assertTrue(is_string($path), "Path is not a string");
+        $this->assertTrue(file_exists($path), "Path does not exist");
+        $this->assertTrue(is_readable($path), "Path is not readable");
+        $this->assertTrue(is_file($path), "Path is not a file");
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionModuleEmpty()
+    {
+        $path = $this->pf->find();
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionModuleNotString()
+    {
+        $path = $this->pf->find(['foo' => 'bar']);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionPathNotString()
+    {
+        $path = $this->pf->find('Foo', ['foo' => 'bar']);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testFindExceptionPathNotExist()
+    {
+        $path = $this->pf->find('Foo', 'some_non_existing_file.csv');
+    }
+
+}
