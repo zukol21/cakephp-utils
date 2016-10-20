c2542d58527fc94ad8506f10f46c27b9c5e3ec89
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-10-20 10:00:19 +0300

    Updated doc-blocks (task #2833)

--- ConfigPathFinder.php
+++ ConfigPathFinder.php
@@ -3,6 +3,17 @@ namespace CsvMigrations\PathFinder;
 
 use Cake\Core\Configure;
 
+/**
+ * ConfigPathFinder Class
+ *
+ * This path finder is here to assist with finding
+ * the paths to the module configuration files.  If
+ * no $path is specified, then the path to the
+ * default configuration file (config.ini) is
+ * returned.
+ *
+ * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
+ */
 class ConfigPathFinder extends BasePathFinder
 {
     protected $pathConfigKey = 'CsvMigrations.migrations.path';
--- MigrationPathFinder.php
+++ MigrationPathFinder.php
@@ -3,6 +3,17 @@ namespace CsvMigrations\PathFinder;
 
 use Cake\Core\Configure;
 
+/**
+ * MigrationPathFinder Class
+ *
+ * This path finder is here to assist with finding
+ * the paths to the module migration files.  If
+ * no $path is specified, then the path to the
+ * default migration file (migration.csv) is
+ * returned.
+ *
+ * @author Leonid Mamchenkov <l.mamchenkov@qobo.biz>
+ */
 class MigrationPathFinder extends BasePathFinder
 {
     protected $pathConfigKey = 'CsvMigrations.migrations.path';
