c6e1b3d07c52bcd5610418d549a69497f9f0a5da
Andrey Vystavkin <jujav4ik@gmail.com>
2016-11-11 11:09:29 +0200

    adding code coverage for ConfigurationTrait and association labels (task #3007)

--- Ini/ParserTest.php
+++ Ini/ParserTest.php
@@ -28,4 +28,16 @@ class ParserTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(empty($result['table']['display_field']), "Parser missed 'display_field' key");
         $this->assertEquals('name', $result['table']['display_field'], "Parser misinterpreted 'display_field' value");
     }
+
+    public function testParseFromPathTestingArrays()
+    {
+        $file = dirname(dirname(dirname(__DIR__))) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS . 'Foo' . DS . 'array_in_config.ini';
+        $parser = new Parser();
+        $result = $parser->parseFromPath($file);
+
+        $this->assertTrue(is_array($result), 'Return data from parser isn\'t array type');
+        $this->assertArrayHasKey('associations', $result, "No associations found in the table config");
+        $this->assertArrayHasKey('association_labels', $result['associations'], "No associations found in the table config");
+        $this->assertTrue(is_array($result['associations']['association_labels']), "Associations label is not an array");
+    }
 }
