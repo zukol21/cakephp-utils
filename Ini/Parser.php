85c7bf12787d4f48b49de543c17033d528b77d7a
George Constantinou <g.constantinou@qobo.biz>
2017-02-13 10:46:26 +0200

    moved method that retrieves fields ini parameters, to INI parser (task #3316)

--- Ini/Parser.php
+++ Ini/Parser.php
@@ -37,4 +37,76 @@ class Parser extends AbstractIniParser
 
         return $result;
     }
+
+    /**
+     * Get fields ini parameters
+     *
+     * Retrieves and returns fields ini parameters. It accepts a single parameter
+     * in a string format or multiple parameters in an array format. If no parameters
+     * are requested then the full field ini configuration is returned.
+     *
+     * If no field ini configration was found then the return value will be an empty
+     * array if multiple parameters were requested or null if one parameter was
+     * requested.
+     *
+     * @param string $path Fields ini path
+     * @param string $field Field name
+     * @param string|array|null $params Field ini parameters
+     * @return string|array|null
+     */
+    public function getFieldsIniParams($path, $field, $params = null)
+    {
+        if (is_array($params) && 1 === count($params)) {
+            $params = current($params);
+        }
+        $result = is_string($params) ? null : [];
+
+        if (!is_string($path)) {
+            throw new \RuntimeException('Path must be a string');
+        }
+
+        // skip if path or field values not provided
+        if (empty($path) || empty($field)) {
+            return $result;
+        }
+
+        $parsed = [];
+        try {
+            $parsed = $this->parseFromPath($path);
+        } catch (\Exception $e) {
+            //
+        }
+
+        // return empty result if fields ini was not parsed
+        if (empty($parsed)) {
+            return $result;
+        }
+
+        // return empty result if current field has no ini configuration
+        if (empty($parsed[$field])) {
+            return $result;
+        }
+
+        // return all configuration for current field if no specific params requested
+        if (empty($params)) {
+            return $parsed[$field];
+        }
+
+        if (is_string($params)) {
+            $result = !empty($parsed[$field][$params]) ? $parsed[$field][$params] : $result;
+
+            return $result;
+        }
+
+        // pick only requested params for current field ini configuration
+        foreach ($params as $param) {
+            if (empty($parsed[$field][$param])) {
+                continue;
+            }
+
+            $result[$param] = $parsed[$field][$param];
+        }
+
+        return $result;
+    }
 }
