90de15889010f808e11bf219d5d311c6780fad03
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-04-10 18:04:50 +0300

    Adjusted parser unit tests (task #3495)

--- tests/TestCase/ModuleConfig/Parser/Ini/ConfigParserTest.php
+++ tests/TestCase/ModuleConfig/Parser/Ini/ConfigParserTest.php
@@ -3,16 +3,16 @@ namespace Qobo\Utils\Test\TestCase\ModuleConfig\Parser\Ini;
 
 use Cake\Core\Configure;
 use PHPUnit_Framework_TestCase;
-use Qobo\Utils\ModuleConfig\Parser\Ini\Parser;
+use Qobo\Utils\ModuleConfig\Parser\Ini\ConfigParser;
 
-class ParserTest extends PHPUnit_Framework_TestCase
+class ConfigParserTest extends PHPUnit_Framework_TestCase
 {
     protected $parser;
     protected $dataDir;
 
     protected function setUp()
     {
-        $this->parser = new Parser();
+        $this->parser = new ConfigParser();
         $this->dataDir = dirname(dirname(dirname(dirname(__DIR__)))) . DS . 'data' . DS . 'Modules' . DS;
         Configure::write('CsvMigrations.modules.path', $this->dataDir);
     }
