f9c5aa55fda5167c70a91508014d36b6525248c8
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-04-10 17:54:41 +0300

    Added `getErrors()` to AbstractIniParser (task #3495)
    * Added protected $errors property
    * Added `getErrors()` method to adhere to the interface
    * Minor code cleanup
    * Minor improvements to parameter checking

--- src/ModuleConfig/Parser/AbstractParser.php
+++ src/ModuleConfig/Parser/AbstractParser.php
@@ -1,10 +1,17 @@
 <?php
 namespace Qobo\Utils\ModuleConfig\Parser;
 
+use InvalidArgumentException;
+
 abstract class AbstractParser implements ParserInterface
 {
     /**
-     * Parsing options
+     * @var array $errors List of errors from last parsing
+     */
+    protected $errors = [];
+
+    /**
+     * @var array $options Parsing options
      */
     protected $options = [];
 
@@ -13,6 +20,7 @@ abstract class AbstractParser implements ParserInterface
      *
      * Parses a given file according to the specified options
      *
+     * @throws InvalidArgumentException when file is not readable or not valid
      * @param string $path    Path to file
      * @param array  $options Options for parsing
      * @return array
@@ -20,16 +28,37 @@ abstract class AbstractParser implements ParserInterface
     abstract public function parse($path, array $options = []);
 
     /**
+     * Get parser errors
+     *
+     * @return array List of errors from last parsing
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
      * Validate path
      *
-     * @throws \InvalidArgumentException If $path does not exist or is not readable
+     * @throws InvalidArgumentException If $path does not exist or is not readable
      * @param string $path Path to validate
      * @return void
      */
     protected function validatePath($path)
     {
+        $path = (string)$path;
+        $path = trim($path);
+
+        if (empty($path)) {
+            $error = 'Path cannot be empty';
+            $this->errors[] = $error;
+            throw new InvalidArgumentException($error);
+        }
+
         if (!is_readable($path)) {
-            throw new \InvalidArgumentException("Path does not exist or is not readable: $path");
+            $error = "Path does not exist or is not readable: $path";
+            $this->errors[] = $error;
+            throw new InvalidArgumentException($error);
         }
     }
 }
