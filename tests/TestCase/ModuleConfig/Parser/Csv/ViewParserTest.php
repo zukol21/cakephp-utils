d50f1a22afe2faf88ecdf1cf5c4e8d6a66e1f2ea
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-04-23 18:48:33 +0300

    More parser unit tests (task #3495)
    Make sure we cover both add/edit/view and index view parsing
    properly.

--- tests/TestCase/ModuleConfig/Parser/Csv/ViewParserTest.php
+++ tests/TestCase/ModuleConfig/Parser/Csv/ViewParserTest.php
@@ -19,6 +19,7 @@ class ViewParserTest extends PHPUnit_Framework_TestCase
 
     public function testParse()
     {
+        // add, edit, view
         $file = $this->dataDir . 'Foo' . DS . 'views' . DS . 'view.csv';
         $result = $this->parser->parse($file);
 
@@ -30,5 +31,18 @@ class ViewParserTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Details', $result[0][0], "Parser missed panel name in first element");
         $this->assertEquals('status', $result[0][1], "Parser missed first field in first element");
         $this->assertEquals('type', $result[0][2], "Parser missed second field in first element");
+
+        // index
+        $file = $this->dataDir . 'Foo' . DS . 'views' . DS . 'index.csv';
+        $result = $this->parser->parse($file);
+
+        $this->assertTrue(is_array($result), "Parser returned a non-array");
+        $this->assertFalse(empty($result), "Parser returned empty result");
+        $this->assertTrue(is_array($result[0]), "Parser returned a non-array first element");
+        $this->assertFalse(empty($result[0]), "Parser returned a non-array first element");
+        $this->assertEquals(1, count($result[0]), "Parser returned incorrect number of items in first element");
+        $this->assertEquals('status', $result[0][0], "Parser missed first field in first element");
+        $this->assertEquals('type', $result[1][0], "Parser missed first field in second element");
+
     }
 }
