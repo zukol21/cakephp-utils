67848e9168b20a56f3a14890916cf1eb63f0a506
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-06-13 23:15:22 +0300

    Avoid configuration duplicates (task #4002)

--- config/file_storage.php
+++ config/file_storage.php
@@ -1,47 +1,34 @@
 <?php
+// Thumbnail sizes
+$thumbnailSizes = [
+    'huge' => [
+        'thumbnail' => ['width' => 2000, 'height' => 2000]
+    ],
+    'large' => [
+        'thumbnail' => ['width' => 1024, 'height' => 1024]
+    ],
+    'medium' => [
+        'thumbnail' => ['width' => 500, 'height' => 500]
+    ],
+    'small' => [
+        'thumbnail' => ['width' => 150, 'height' => 150]
+    ],
+    'tiny' => [
+        'thumbnail' => ['width' => 50, 'height' => 50]
+    ]
+];
+
 // Burzum File-Storage plugin configuration
 return [
     // default thumbnail setup for all $entity->model entities for file_storage
-    'ThumbnailVersions' => [
-        'huge' => [
-            'thumbnail' => ['width' => 2000, 'height' => 2000]
-        ],
-        'large' => [
-            'thumbnail' => ['width' => 1024, 'height' => 1024]
-        ],
-        'medium' => [
-            'thumbnail' => ['width' => 500, 'height' => 500]
-        ],
-        'small' => [
-            'thumbnail' => ['width' => 150, 'height' => 150]
-        ],
-        'tiny' => [
-            'thumbnail' => ['width' => 50, 'height' => 50]
-        ]
-    ],
+    'ThumbnailVersions' => $thumbnailSizes,
     'FileStorage' => [
         'imageProcessing' => true,
         'pathBuilderOptions' => ['pathPrefix' => '/uploads'],
         'association' => 'UploadDocuments',
         'defaultImageSize' => 'huge',
         'imageSizes' => [
-            'file_storage' => [
-                'huge' => [
-                    'thumbnail' => ['width' => 2000, 'height' => 2000]
-                ],
-                'large' => [
-                    'thumbnail' => ['width' => 1024, 'height' => 1024]
-                ],
-                'medium' => [
-                    'thumbnail' => ['width' => 500, 'height' => 500]
-                ],
-                'small' => [
-                    'thumbnail' => ['width' => 150, 'height' => 150]
-                ],
-                'tiny' => [
-                    'thumbnail' => ['width' => 50, 'height' => 50]
-                ]
-            ]
+            'file_storage' => $thumbnailSizes
         ]
     ]
 ];
