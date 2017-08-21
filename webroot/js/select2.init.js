aaf4f612d011a73ae7fed258bf076bcb245b27dd
George Constantinou <georgec20001@gmail.com>
2017-08-21 12:02:09 +0300

    Fix for select2 working within modals (task #4371)

--- webroot/js/select2.init.js
+++ webroot/js/select2.init.js
@@ -8,4 +8,8 @@
             return text;
         }
     });
+
+    // fix for select2 search not working within bootstrap modals
+    // @link https://github.com/select2/select2/issues/1436#issuecomment-21028474
+    $.fn.modal.Constructor.prototype.enforceFocus = function () {};
 })(jQuery);
