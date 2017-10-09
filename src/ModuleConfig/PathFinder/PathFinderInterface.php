8cd0adb1559df2bea0f632d1754b3de9096a4702
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-10-09 12:46:57 +0300

    Switched license from GPLv2 to MIT (task #4283)
    * Updated composer.json license and authors
    * Added copyright to all PHP files in src/

--- src/ModuleConfig/PathFinder/PathFinderInterface.php
+++ src/ModuleConfig/PathFinder/PathFinderInterface.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Qobo\Utils\ModuleConfig\PathFinder;
 
 /**
