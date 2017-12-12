3501fce8e4dc6ed32edac40015d203ea953d7831
Michael Stepanov <michael@stepanoff.org>
2017-12-12 16:56:44 +0200

    Added new locker to lock background scrits - task 5054

---
+++ src/Utility/Locker/FileLocker.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Qobo\Utils\Utility\Locker;
+
+use NinjaMutex\Lock\FlockLock;
+use Qobo\Utils\Utility\Locker\BaseLocker;
+
+class FileLocker extends BaseLocker
+{
+    /**
+     * @var $lockDir
+     */
+    private $lockDir = '/tmp';
+    
+    /**
+     * __construct method
+     *
+     * @param string $key to use to lock operation
+     * @param string $dir to store lock files
+     * @return void
+     */
+    public function __construct($key, $dir=null)
+    {
+        if (!empty($dir)) {
+            $this->lockDir = $dir;
+        }
+
+        $lock = new FlockLock($this->lockDir);
+
+        parent::__construct($key, $lock);
+    }
+}
