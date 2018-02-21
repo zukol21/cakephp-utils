413c5b6b462cfe0744724ef199c3140ffa75aa16
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-02-21 15:01:30 +0200

    Added ConfigParser defaults from V1 to V2 (task #3495)

--- src/ModuleConfig/Parser/V2/Json/ConfigParser.php
+++ src/ModuleConfig/Parser/V2/Json/ConfigParser.php
@@ -11,6 +11,8 @@
  */
 namespace Qobo\Utils\ModuleConfig\Parser\V2\Json;
 
+use stdClass;
+
 /**
  * Config JSON Parser
  *
@@ -30,4 +32,98 @@ class ConfigParser extends AbstractJsonParser
      * @var string|\stdClass $schema JSON schema
      */
     protected $schema = 'file://' . __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Schema' . DIRECTORY_SEPARATOR . 'config.json';
+
+    /**
+     * Default sections and configuration values
+     *
+     * @var array
+     */
+    protected $defaults = [
+        'table' => [
+            'icon' => 'cube',
+            'searchable' => true,
+            'lookup_fields' => [],
+            'typeahead_fields' => [],
+            'basic_search_fields' => [],
+            'allow_reminders' => [],
+            'translatable' => false,
+            'permissions_parent_modules' => [],
+        ],
+        'virtualFields' => [],
+        'associations' => [
+            'hide_associations' => [],
+        ],
+        'associationLabels' => [],
+        'notifications' => [
+            'enable' => false,
+            'ignored_fields' => [],
+        ],
+        'manyToMany' => [
+            'modules' => [],
+        ],
+    ];
+
+    /**
+     * Merge with default values
+     *
+     * @param object $data Data to merge with defaults
+     * @return object
+     */
+    protected function mergeWithDefaults($data = null)
+    {
+        // Set defaults
+        foreach ($this->defaults as $section => $options) {
+            // Make sure the section exists
+            if (!property_exists($data, $section)) {
+                $data->$section = new stdClass();
+            }
+            // Make sure default values are set
+            foreach ($options as $option => $value) {
+                if (!property_exists($data->$section, $option)) {
+                    $data->$section->$option = $value;
+                }
+            }
+        }
+
+        // Convert CSV string values to arrays
+        $data->table->lookup_fields = $this->csv2array($data->table->lookup_fields);
+        $data->table->typeahead_fields = $this->csv2array($data->table->typeahead_fields);
+        $data->table->permissions_parent_modules = $this->csv2array($data->table->permissions_parent_modules);
+        $data->table->basic_search_fields = $this->csv2array($data->table->basic_search_fields);
+        $data->table->allow_reminders = $this->csv2array($data->table->allow_reminders);
+        $data->associations->hide_associations = $this->csv2array($data->associations->hide_associations);
+        $data->notifications->ignored_fields = $this->csv2array($data->notifications->ignored_fields);
+        $data->manyToMany->modules = $this->csv2array($data->manyToMany->modules);
+
+        // [table]display_field
+        if (!property_exists($data->table, 'display_field')) {
+            $this->warnings = array_merge($this->warnings, ["'display_field' is not set in 'table' section"]);
+        }
+
+        $virtualFields = json_decode(json_encode($data->virtualFields), true);
+        foreach ($virtualFields as $virtualField => $realFields) {
+            $data->virtualFields->$virtualField = $this->csv2array($realFields);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Convert a comma-separated string to array
+     *
+     * If provided $csv is not a string, return as is.
+     *
+     * @param string $csv String to convert
+     * @return array
+     */
+    protected function csv2array($csv)
+    {
+        if (!is_string($csv)) {
+            return $csv;
+        }
+
+        $result = explode(',', $csv);
+
+        return $result;
+    }
 }
