71c127e280385993a9ebc0f295d42e14162d1737
Demetris Papademetriou <d.papademetrioug13@gmail.com>
2018-11-16 16:15:46 +0200

    Added two useful methods to use during phpunit test to test private methods (task #8430)

---
+++ src/TestSuite/Utility.php
@@ -0,0 +1,45 @@
+<?php
+namespace Qobo\Utils\TestSuite;
+
+/**
+ * Class for useful methods in regards testing
+ */
+class Utility
+{
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Return method back
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public static function callPrivateMethod(&$object, $methodName, $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
+     * Call protected/private method for static class.
+     *
+     * @param string &$object    Return method back
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public static function callStaticPrivateMethod(String $object, String $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass($object);
+
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($reflection, $parameters);
+    }
+}
