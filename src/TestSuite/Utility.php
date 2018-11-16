5fd800d9ddcbadee7258b27326c2b56c295aa06d
Demetris Papademetriou <d.papademetrioug13@gmail.com>
2018-11-16 16:42:28 +0200

    Removed an unused '&' (task #8430)

--- src/TestSuite/Utility.php
+++ src/TestSuite/Utility.php
@@ -9,13 +9,13 @@ class Utility
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object    Return method back
+     * @param object $object    Return method back
      * @param string $methodName Method name to call
      * @param array  $parameters Array of parameters to pass into method.
      *
      * @return mixed Method return.
      */
-    public static function callPrivateMethod(&$object, $methodName, $parameters = [])
+    public static function callPrivateMethod($object, $methodName, $parameters = [])
     {
         $reflection = new \ReflectionClass(get_class($object));
         $method = $reflection->getMethod($methodName);
@@ -27,7 +27,7 @@ class Utility
     /**
      * Call protected/private method for static class.
      *
-     * @param string &$object    Return method back
+     * @param string $object    Return method back
      * @param string $methodName Method name to call
      * @param array  $parameters Array of parameters to pass into method.
      *
