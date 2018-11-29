58e610b1425be83d591ae63658ad071987d47731
Timur Asaliev <t.asaliev@qobo.biz>
2018-11-29 15:34:35 +0200

    Update view json unit test and schema

--- tests/TestCase/ModuleConfig/Parser/V2/Json/ViewParserTest.php
+++ tests/TestCase/ModuleConfig/Parser/V2/Json/ViewParserTest.php
@@ -3,15 +3,31 @@ namespace Qobo\Utils\Test\TestCase\ModuleConfig\Parser\V2\Json;
 
 use Cake\Core\Configure;
 use InvalidArgumentException;
+use JsonSchema\Exception\ValidationException;
+
 use PHPUnit\Framework\TestCase;
 use Qobo\Utils\ModuleConfig\Parser\V2\Json\ViewParser;
 use Qobo\Utils\Utility\Convert;
 
 class ViewParserTest extends TestCase
 {
+    /**
+     * Config parser
+     * @var \Qobo\Utils\ModuleConfig\Parser\V2\Json\ViewParser
+     */
     protected $parser;
+
+    /**
+     * Path to modules
+     * @var string
+     */
     protected $dataDir;
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return void
+     */
     public function setUp()
     {
         $this->parser = new ViewParser();
@@ -21,33 +37,42 @@ class ViewParserTest extends TestCase
         Configure::write('ModuleConfig.classMapVersion', 'V2');
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function tearDown()
     {
         unset($this->dataDir);
         unset($this->parser);
     }
 
+    /**
+     * Tests valid views json
+     *
+     * @return void
+     */
     public function testParse(): void
     {
         $file = $this->dataDir . DS . 'Foo' . DS . 'views' . DS . 'add.json';
 
         $result = $this->parser->parse($file);
-        $result = Convert::objectToArray($result);
-
         $this->assertNotEmpty($result);
-        $this->assertArrayHasKey('items', $result);
-        foreach ($result['items'] as $rowFields) {
-            $this->assertGreaterThan(0, count($rowFields));
-            $this->assertLessThan(14, count($rowFields));
-        }
+        $this->assertEmpty($this->parser->getErrors());
     }
 
+    /**
+     * Tests invalid views json
+     *
+     * @return void
+     */
     public function testParseInvalid(): void
     {
-        $this->expectException(InvalidArgumentException::class);
-
         $file = $this->dataDir . DS . 'Foo' . DS . 'views' . DS . 'invalid.json';
 
-        $this->parser->parse($file);
+        try {
+            $this->parser->parse($file);
+        } catch (InvalidArgumentException $e) {
+            $this->assertContains('Error validating /items/0: There must be a maximum of 13 items in the array', $e->getMessage());
+        }
     }
 }
