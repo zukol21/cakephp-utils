e9f8b1b876e0950a6df2c4bf6447867fff4056a2
Timur Asaliev <t.asaliev@qobo.biz>
2018-11-29 13:02:43 +0200

    Add unit test for ConfigParser (task #8712)

---
+++ tests/TestCase/ModuleConfig/Parser/V2/Json/ConfigParserTest.php
@@ -0,0 +1,125 @@
+<?php
+namespace Qobo\Utils\Test\TestCase\ModuleConfig\Parser\V2\Json;
+
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Qobo\Utils\ModuleConfig\Parser\V2\Json\ConfigParser;
+use Qobo\Utils\Utility\Convert;
+
+class ConfigParserTest extends TestCase
+{
+    /**
+     * Config parser
+     * @var \Qobo\Utils\ModuleConfig\Parser\V2\Json\ConfigParser
+     */
+    protected $parser;
+
+    /**
+     * Path to modules
+     * @var string
+     */
+    protected $dataDir;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        $this->parser = new ConfigParser();
+        $this->dataDir = TESTS . 'data' . DS . 'Modules';
+
+        Configure::write('CsvMigrations.modules.path', $this->dataDir);
+        Configure::write('ModuleConfig.classMapVersion', 'V2');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function tearDown()
+    {
+        unset($this->dataDir);
+        unset($this->parser);
+    }
+
+    /**
+     * Parse a valid config json file.
+     *
+     * @return void
+     */
+    public function testParse(): void
+    {
+        $file = $this->dataDir . DS . 'Foo' . DS . 'config' . DS . 'config.json';
+
+        $result = $this->parser->parse($file);
+        $this->assertNotEmpty($result);
+        $this->assertEmpty($this->parser->getErrors());
+    }
+
+    /**
+     * Parse a valid config json file with ommited default values
+     *
+     * @return void
+     */
+    public function testParseWithoutDefaults(): void
+    {
+        $file = $this->dataDir . DS . 'Foo' . DS . 'config' . DS . 'config_no_defaults.json';
+
+        $result = $this->parser->parse($file);
+        $this->assertNotEmpty($result);
+        $this->assertEmpty($this->parser->getErrors());
+
+        $defaults = [
+            'table' => [
+                'type' => 'module',
+                'display_field' => 'id',
+                'icon' => 'cube',
+                'searchable' => true,
+                'lookup_fields' => [],
+                'typeahead_fields' => [],
+                'basic_search_fields' => [],
+                'allow_reminders' => [],
+                'translatable' => false,
+                'permissions_parent_modules' => [],
+            ],
+            'virtualFields' => [],
+            'associations' => [
+                'hide_associations' => [],
+            ],
+            'associationLabels' => [],
+            'notifications' => [
+                'enable' => false,
+                'ignored_fields' => [],
+            ],
+            'manyToMany' => [
+                'modules' => [],
+            ],
+        ];
+
+        $defaults = Hash::flatten($defaults);
+        $result = Convert::objectToArray($result);
+        $result = Hash::flatten($result);
+
+        foreach ($defaults as $key => $value) {
+            $this->assertArrayHasKey($key, $result, "Element `{$key}` is missing, should be defaulted by the schema.");
+            $this->assertEquals($value, $result[$key], "Default value of `${key}` doesn't match");
+        }
+    }
+
+    /**
+     * Parse an invalid config json file.
+     *
+     * @return void
+     */
+    public function testParseInvalid(): void
+    {
+        $file = $this->dataDir . DS . 'Foo' . DS . 'config' . DS . 'config_invalid.json';
+
+        try {
+            $this->parser->parse($file);
+        } catch (InvalidArgumentException $e) {
+            $this->assertContains('Error validating /table', $e->getMessage());
+        }
+    }
+}
