bed4a05dd059e4d1a0182afedf4116205a10a94f
Timur Asaliev <t.asaliev@qobo.biz>
2018-11-29 13:22:20 +0200

    Update json schema linting unit test (task #8712)

--- tests/TestCase/ModuleConfig/Parser/Schema/SchemaTest.php
+++ tests/TestCase/ModuleConfig/Parser/Schema/SchemaTest.php
@@ -1,50 +1,104 @@
 <?php
 namespace Qobo\Utils\Test\TestCase\ModuleConfig\Parser\Schema;
 
-use DirectoryIterator;
+use Cake\Filesystem\Folder;
 use PHPUnit\Framework\TestCase;
+use Seld\JsonLint\JsonParser;
+use Seld\JsonLint\ParsingException;
 
 class SchemaTest extends TestCase
 {
     /**
-     * Lint JSON files
-     *
-     * This is a temporary test until we integrate a proper
-     * JSON linting tool.  At a very minimum we need to make
-     * sure that the schema files are parseable.
+     * Path to schema files
+     * @var string
      */
-    public function testJsonLint(): void
+    protected $schemaPath;
+
+    /**
+     * List of json schema files
+     * @var \Seld\JsonLint\JsonParser
+     */
+    protected $linter;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp(): void
     {
+        parent::setUp();
+
         // All your dir are belong to us!
-        $schemaPath = dirname(dirname(dirname(dirname(dirname(__DIR__)))));
-        $schemaPath .= DIRECTORY_SEPARATOR . 'src';
-        $schemaPath .= DIRECTORY_SEPARATOR . 'ModuleConfig';
-        $schemaPath .= DIRECTORY_SEPARATOR . 'Parser';
-        $schemaPath .= DIRECTORY_SEPARATOR . 'Schema';
+        $this->schemaPath = implode(DIRECTORY_SEPARATOR, [
+            dirname(dirname(dirname(dirname(dirname(__DIR__))))),
+            'src', 'ModuleConfig', 'Parser', 'Schema'
+        ]);
 
-        $this->assertTrue(file_exists($schemaPath), "Path does not exist: $schemaPath");
-        $this->assertTrue(is_dir($schemaPath), "Path is not a directory: $schemaPath");
-        $this->assertTrue(is_readable($schemaPath), "Path is not readable: $schemaPath");
+        $this->linter = new JsonParser;
+    }
 
-        foreach (new DirectoryIterator($schemaPath) as $file) {
-            if ($file->isDot() || $file->isDir()) {
-                continue;
-            }
-            if ($file->getExtension() !== 'json') {
-                continue;
-            }
-            $realPath = $file->getRealPath();
-            if (empty($realPath)) {
-                continue;
-            }
-            $content = file_get_contents($realPath);
-            if (empty($content)) {
-                $content = '';
+    /**
+     * {@inheritDoc}
+     */
+    public function tearDown(): void
+    {
+        unset($this->schemaPath);
+        unset($this->linter);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Check the folder with json schemas.
+     *
+     * This unit test also serves as a data provider for `testLintJsonSchemas`
+     * method
+     *
+     * @return string[] List of json schema files
+     */
+    public function testSchemaFolder(): array
+    {
+        $this->assertTrue(file_exists($this->schemaPath), "Path does not exist: $this->schemaPath");
+        $this->assertTrue(is_dir($this->schemaPath), "Path is not a directory: $this->schemaPath");
+        $this->assertTrue(is_readable($this->schemaPath), "Path is not readable: $this->schemaPath");
+
+        $dir = new Folder($this->schemaPath);
+
+        return $dir->read(false, true, true)[1];
+    }
+
+    /**
+     * Lint an array of json schemas
+     *
+     * @depends testSchemaFolder
+     * @param string[] $schemaFiles Array of paths to json schema files.
+     * @return void
+     */
+    public function testLintJsonSchemas(array $schemaFiles): void
+    {
+        foreach ($schemaFiles as $file) {
+            $contents = '';
+            if (($contents = file_get_contents($file)) === false) {
+                $this->fail("Could not read: `$file`");
             }
 
-            $this->assertFalse(empty($content), "Empty file or failed to read: " . $realPath);
-            $data = json_decode($content);
-            $this->assertFalse(empty($data), "Empty structure or failed to parse: " . $realPath);
+            $result = $this->linter->lint((string)$contents, JsonParser::DETECT_KEY_CONFLICTS);
+
+            $this->assertNotInstanceOf(ParsingException::class, $result, $this->formatLinterError($file, $result));
+            $this->assertNull($result);
         }
+    }
+
+    /**
+     * Helper function which formats a `ParsingException` message if one is
+     * given.
+     *
+     * @param string $file File location
+     * @param ParsingException|null $result Parsing exception
+     * @return string Formatted error message if `$result` not null, empty
+     * string otherwise
+     */
+    protected function formatLinterError(string $file, ?ParsingException $result): string
+    {
+        return ($result instanceof ParsingException) ? $file . "\n" . $result->getMessage() : '';
     }
 }
