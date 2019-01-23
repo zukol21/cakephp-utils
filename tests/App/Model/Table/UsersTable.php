6ef6d4d3bff043ae00673ee9b8fd79639dffe8ef
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-23 11:38:33 +0200

    Add user model for tests (task #9585)

---
+++ tests/App/Model/Table/UsersTable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Qobo\Utils\Test\App\Model\Table;
+
+use Cake\ORM\Table;
+
+class UsersTable extends Table
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize(array $config)
+    {
+    }
+}
