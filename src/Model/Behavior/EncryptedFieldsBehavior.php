d7da8247cc110bf568304d6b6076bdefe6f552b9
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-23 11:20:01 +0200

    Add `beforeSave` event (task #9585)

--- src/Model/Behavior/EncryptedFieldsBehavior.php
+++ src/Model/Behavior/EncryptedFieldsBehavior.php
@@ -1,6 +1,9 @@
 <?php
 namespace Qobo\Utils\Model\Behavior;
 
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
 use Cake\ORM\Behavior;
 
 /**
@@ -31,5 +34,29 @@ class EncryptedFieldsBehavior extends Behavior
     public function initialize(array $config)
     {
         $this->setConfig($config);
+    }
+
+    /**
+     * Callback to never really delete a record but instead mark it as `trashed`.
+     *
+     * @param \Cake\Event\Event $event The beforeDelete event that was fired.
+     * @param \Cake\Datasource\EntityInterface $entity The entity to be deleted.
+     * @param \ArrayObject $options Options.
+     * @return void
+     */
+    public function beforeSave(Event $event, EntityInterface $entity, ArrayObject $options): void
+    {
+        $entity = $this->encrypt($entity);
+    }
+
+    /**
+     * Encrypts the fields.
+     *
+     * @param \Cake\Datasource\EntityInterface $entity Entity object.
+     * @return \Cake\Datasource\EntityInterface Entity object.
+     */
+    public function encrypt(EntityInterface $entity): EntityInterface
+    {
+        return $entity;
     }
 }
