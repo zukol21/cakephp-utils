54e4f7b181c3c92abc1ede9554f8e5ba407cf967
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-24 10:04:42 +0200

    Decrease nesting in `decryptEntityField` method (task #9585)

--- src/Model/Behavior/EncryptedFieldsBehavior.php
+++ src/Model/Behavior/EncryptedFieldsBehavior.php
@@ -236,29 +236,29 @@ class EncryptedFieldsBehavior extends Behavior
      */
     public function decryptEntityField(EntityInterface $entity, string $field)
     {
-        if ($this->canDecryptField($entity, $field)) {
-            $encryptionKey = $this->getConfig('encryptionKey');
-            $base64 = $this->getConfig('base64');
-            $encoded = $entity->get($field);
-            if (empty($encoded) || $encoded === false) {
-                return null;
-            }
-
-            if ($base64 === true) {
-                $encoded = base64_decode($encoded, true);
-                if ($encoded === false) {
-                    return null;
-                }
-            }
-            $decrypted = Security::decrypt($encoded, $encryptionKey);
-            if ($decrypted === false) {
-                throw new RuntimeException("Unable to decypher `{$field}`. Check your enryption key.");
-            }
-
-            return $decrypted;
+        if (!$this->canDecryptField($entity, $field)) {
+            return null;
         }
 
-        return null;
+        $encryptionKey = $this->getConfig('encryptionKey');
+        $base64 = $this->getConfig('base64');
+        $encoded = $entity->get($field);
+        if (empty($encoded) || $encoded === false) {
+            return null;
+        }
+
+        if ($base64 === true) {
+            $encoded = base64_decode($encoded, true);
+            if ($encoded === false) {
+                return null;
+            }
+        }
+        $decrypted = Security::decrypt($encoded, $encryptionKey);
+        if ($decrypted === false) {
+            throw new RuntimeException("Unable to decypher `{$field}`. Check your enryption key.");
+        }
+
+        return $decrypted;
     }
 
     /**
